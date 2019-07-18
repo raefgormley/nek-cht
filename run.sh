1ae164c4052a8db2fbe619e128ec2017ca585076
raefgormley <51417525+raefgormley@users.noreply.github.com>
2019-07-18 17:00:44 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #EDIT ALL CASE FILES AND INPUT FILES
 #sed -i 's/letters .*/letters abc/' testfile # change content in front of keyword letters to desired value
 
-
 #pull changes from github
 git pull origin bejan84-history-points --no-edit #--no-edit gets around needing to enter a message explaining the merge
 
