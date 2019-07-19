54aad0be630625d67d8c61a74be3220971d6f483
raefgormley <51417525+raefgormley@users.noreply.github.com>
2019-07-19 12:27:30 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #pull changes from github
 git pull origin bejan84-history-points --no-edit
 
-#make scripts runnable
+#make first scripts runnable
 chmod -x ./rootAnswers
+chmod -x ./copyData
+
+#take user input to name session
+read -p "enter session name: " sessionName
+path='/data/RaefData/'$sessionName
+
+#run rootAnswers to create folder in data and copy files over
+./rootAnswers
+
+#switch to data directory
+cd $path
+
+#make answers and compile runnable
+chmod -x ./answers
+chmod -x ./compile
+
+#run answers to compile case
+./answers
+
+#run case
+nekbmpi $sessionName 4
